f0d6ac46c828d064de665bdeba08abf7a9670c7d
unnimayaprajil <unnimayasubramanyan@gmail.com>
2024-04-25 12:53:47 +0530

    variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,48 @@
-alert("hellooo"); 
+// var
+// var a=10
+
+// function myVarFunc(){
+//     console.log('a',a)
+//     var b=20;
+//     a=30;
+// }
+// myVarFunc();
+// function myVarFunc2(){
+//     console.log(' fun2 a',a)
+//     console.log('b',b)
+// }
+// myVarFunc2();
+
+// let
+// let c=10
+// function myVarFunc(){
+//     let c =20;
+//     console.log('let c',c)
+//     let d=20;
+//     // c=30;
+// }
+// myVarFunc();
+// c=30;
+// console.log('c out',c)
+
+// function myVarFunc2(){
+//     console.log(' fun2 let c',c)
+//     console.log(' let d',d)
+// }
+// myVarFunc2();
+
+//const
+// const c=10
+// function myVarFunc(){
+//     const c =20;
+//     console.log('const c',c)
+//     const d=20;
+  
+// }
+// myVarFunc();
+// c=30
+// console.log('c out',c)
+
+a=10;
+console.log('a',a)
+var a
