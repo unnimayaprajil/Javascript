2c654f31fd4e94aeb319b1e2e2a5cfda7f939cac
unnimayaprajil <unnimayasubramanyan@gmail.com>
2024-05-05 17:52:45 +0530

    javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-alert("hellooo"); 
+const myFunction = ()=>{
+setTimeout(()=>{
+    console.log('Timeout hello')
+},5000)
+}
+async function myFun2(){
+    console.log('helloo1')
+   const data = await myFunction();
+   console.log('data',data)
+    console.log('helloo2')
+}
+myFun2()
